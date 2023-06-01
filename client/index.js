4b5567376a69105a3a3f9f7134dea43b1cb0c776
Kif_Francis <kif.zig@gmail.com>
2023-05-31 22:48:17 -0500

    Set up connection to mysql db

--- client/index.js
+++ client/index.js
@@ -1,9 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  loadHTMLTable([]);
+  fetch("http://localhost:5000/getAll")
+    .then((response) => response.json())
+    .then((data) => loadHTMLTable(data["data"]));
 });
+
+const addBtn = document.querySelector("#add-name-btn");
+addBtn.onclick = function () {
+  const nameInput = document.querySelector("#name-input");
+  const name = nameInput.value;
+  nameInput.value = "";
+  fetch('http://localhost:5000/insert', {
+    headers: {
+      'Content-type'
+      // Pick up hear again at 40:03 -- my app may not be outputting the right {data data[] either}
+    }
+  })
+};
 
 function loadHTMLTable(data) {
   const table = document.querySelector("table tbody");
+  console.log(data);
   // let tableHTML = "";
   if (data.length == 0) {
     table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
--- server/app.js
+++ server/app.js
@@ -4,17 +4,25 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const dbService = require("./dbService");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // create
-app.post("/insert", (request, response) => {});
+app.post("/insert", (request, response) => {
+  console.log(request.body);
+});
 
 // read
 app.get("/getAll", (request, response) => {
-  console.log("test");
-  console.log(request, response);
+  const db = dbService.getDBServiceInstance();
+  const result = db.getAllData();
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log("get " + err));
 });
 
 // update
